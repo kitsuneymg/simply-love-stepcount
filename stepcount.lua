2b722b0d805c4105fff23f518e9cb478de8ed056
kitsuneymg <kitsuneymg@users.noreply.github.com>
2025-01-05 19:33:31 -0600

    Update stepcount.lua
    added a date to stepcount

--- stepcount.lua
+++ stepcount.lua
@@ -3,16 +3,18 @@ local t = {}
 t["ScreenProfileSave"] = Def.ActorFrame {
     ModuleCommand=function(self)
 		local DateFormat = "%04d%02d%02d-%02d%02d%02d"
-		local datestr = DateFormat:format(Year(), MonthOfYear()+1, DayOfMonth(), Hour(), Minute(), Second())
+		local DateFormat2 = "%04d-%02d-%02d %02d:%02d:%02d"
+		local datestr = DateFormat:format(Year(), MonthOfYear()+1, DayOfMonth(), Hour(), Minute(), Second())		
+		local datestr2 = DateFormat2:format(Year(), MonthOfYear()+1, DayOfMonth(), Hour(), Minute(), Second())
 		local profileSlot = {
 				[PLAYER_1] = "ProfileSlot_Player1",
 				[PLAYER_2] = "ProfileSlot_Player2"
 			}		
 		for player in ivalues(GAMESTATE:GetHumanPlayers()) do
-			local pss = STATSMAN:GetCurStageStats():GetPlayerStageStats(player)
 			local notesHitThisGame = 0
 			local content = { }
 			local profileDir  = PROFILEMAN:GetProfileDir(profileSlot[player])
+			for i,stats in pairs( SL[ToEnumShortString(player)].Stages.Stats ) do
 				if stats and stats.column_judgments then
 					for column, judgments in ipairs(stats.column_judgments) do
 						for judgment, judgment_count in pairs(judgments) do
@@ -27,7 +29,8 @@ t["ScreenProfileSave"] = Def.ActorFrame {
 				end
 			end
 			content.Steps = { notesHitThisGame }
-			local filename = "steps-" .. datestr:gsub(':', '') .. ".json"
+			content.DateTime = { datestr2 }
+			local filename = "steps-" .. datestr .. ".json"
 			local f = RageFileUtil.CreateRageFile()		
 			if f:Open(profileDir .. "SL-Steps/" .. filename, 2) then
 				f:Write(JsonEncode(content, true))
