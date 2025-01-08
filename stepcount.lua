84808ca0bf77e21fccdb60fe40c461e26793b3d0
kitsuneymg <kitsuneymg@users.noreply.github.com>
2025-01-07 21:19:52 -0600

    Write file as songs complete
    Also fixing a bug with show held misses enabled and other features of zmod

--- stepcount.lua
+++ stepcount.lua
@@ -1,42 +1,58 @@
 local t = {}
 
-t["ScreenEvaluationSummary"] = Def.ActorFrame {
-	ModuleCommand=function(self)
-	    local DateFormat = "%04d%02d%02d-%02d%02d%02d"
-        local DateFormat2 = "%04d-%02d-%02d %02d:%02d:%02d"
-        local datestr = DateFormat:format(Year(), MonthOfYear()+1, DayOfMonth(), Hour(), Minute(), Second())        
-        local datestr2 = DateFormat2:format(Year(), MonthOfYear()+1, DayOfMonth(), Hour(), Minute(), Second())
-        local profileSlot = {
-                [PLAYER_1] = "ProfileSlot_Player1",
-                [PLAYER_2] = "ProfileSlot_Player2"
-            }        
-        for player in ivalues(GAMESTATE:GetHumanPlayers()) do
-            local notesHitThisGame = 0
-            local content = { }
-            local profileDir  = PROFILEMAN:GetProfileDir(profileSlot[player])
-            for i,stats in pairs( SL[ToEnumShortString(player)].Stages.Stats ) do
-                if stats and stats.column_judgments then
-                    for column, judgments in ipairs(stats.column_judgments) do
-                        for judgment, judgment_count in pairs(judgments) do
-                            -- Early hits are a stored in a table, we want to ignore those for this calculation.
-                            if type(judgment_count) == "number" then
-                                if judgment ~= "Miss" then
-                                    notesHitThisGame = notesHitThisGame + judgment_count
-                                end
+local function write_step_count(filename)
+    local DateFormat = "%04d%02d%02d-%02d%02d%02d"
+    local DateFormat2 = "%04d-%02d-%02d %02d:%02d:%02d"
+    local datestr = DateFormat:format(Year(), MonthOfYear()+1, DayOfMonth(), Hour(), Minute(), Second())        
+    local datestr2 = DateFormat2:format(Year(), MonthOfYear()+1, DayOfMonth(), Hour(), Minute(), Second())
+    local profileSlot = {
+            [PLAYER_1] = "ProfileSlot_Player1",
+            [PLAYER_2] = "ProfileSlot_Player2"
+        }
+    for player in ivalues(GAMESTATE:GetHumanPlayers()) do
+        local notesHitThisGame = 0
+        local content = { }
+        local profileDir  = PROFILEMAN:GetProfileDir(profileSlot[player])
+        for i,stats in pairs( SL[ToEnumShortString(player)].Stages.Stats ) do
+            if stats and stats.column_judgments then
+                for column, judgments in ipairs(stats.column_judgments) do
+                    for judgment, judgment_count in pairs(judgments) do
+                        -- Early hits are a stored in a table, we want to ignore those for this calculation.
+                        if type(judgment_count) == "number" then
+                            if string.len(judgment) == 2 then
+                                notesHitThisGame = notesHitThisGame + judgment_count
                             end
+                        
                         end
                     end
                 end
             end
-            content.Steps = { notesHitThisGame }
-            content.DateTime = { datestr2 }
-            local filename = "steps-" .. datestr .. ".json"
-            local f = RageFileUtil.CreateRageFile()        
-            if f:Open(profileDir .. "SL-Steps/" .. filename, 2) then
-                f:Write(JsonEncode(content, true))
-            end
-            f:destroy()
         end
+        content.Steps = { notesHitThisGame }
+        content.DateTime = { datestr2 }
+        
+        local f = RageFileUtil.CreateRageFile()        
+        if f:Open(profileDir .. "SL-Steps/" .. filename, 2) then
+            f:Write(JsonEncode(content, true))
+        end
+        f:destroy()
+    end
+end
+
+t["filename"] = ""
+
+t["ScreenTitleMenu"] = Def.ActorFrame {
+    ModuleCommand=function(self)
+        local DateFormat = "%04d%02d%02d-%02d%02d%02d"
+        local datestr = DateFormat:format(Year(), MonthOfYear()+1, DayOfMonth(), Hour(), Minute(), Second())        
+        local filename = "steps-" .. datestr .. ".json"
+        t["filename"] = filename
+    end
+}
+
+t["ScreenEvaluationStage"] = Def.ActorFrame {
+	ModuleCommand=function(self)
+	    write_step_count(t["filename"])
     end
 }
 
