543e819f3f46cc74d7c81d54995c0a7ab8e20f1d
kitsuneymg <kitsuneymg@users.noreply.github.com>
2026-01-02 21:09:17 -0600

    UI Improvements
    Make this look more like other plugins with checks and X and less MESSAGE BANNER nonsense

--- stepcount.lua
+++ stepcount.lua
@@ -1,6 +1,8 @@
+local MODULE_TAG = "[Stepcount]"
 local t = {}
 
-local function write_step_count(filename)
+local function write_step_count(filename, actor)
+    local filename = t["filename"]
     local DateFormat = "%04d%02d%02d-%02d%02d%02d"
     local DateFormat2 = "%04d-%02d-%02d %02d:%02d:%02d"
     local datestr = DateFormat:format(Year(), MonthOfYear()+1, DayOfMonth(), Hour(), Minute(), Second())        
@@ -8,6 +10,10 @@ local function write_step_count(filename)
     local profileSlot = {
             [PLAYER_1] = "ProfileSlot_Player1",
             [PLAYER_2] = "ProfileSlot_Player2"
+        }
+    local msgbox = {
+            [PLAYER_1] = actor:GetChild("CountP1"),
+            [PLAYER_2] = actor:GetChild("CountP2")
         }
     for player in ivalues(GAMESTATE:GetHumanPlayers()) do
         local notesHitThisGame = 0
@@ -30,32 +36,73 @@ local function write_step_count(filename)
             f:Write(JsonEncode(content, true))
         end
         f:destroy()
-        SM("Writing step count of " .. notesHitThisGame)
+        local msg = msgbox[player]
+        msg:settext("✔ Steps: " .. notesHitThisGame)
     end
 end
 
 t["filename"] = ""
 
 t["ScreenTitleMenu"] = Def.ActorFrame {
+    InitCommand = function(self)
+        self:xy(10, 35):zoom(0.8):halign(1)
+    end,
     ModuleCommand=function(self)
+        local bmt = self:GetChild("Status")
+        if not bmt then return end
         local DateFormat = "%04d%02d%02d-%02d%02d%02d"
         local datestr = DateFormat:format(Year(), MonthOfYear()+1, DayOfMonth(), Hour(), Minute(), Second())        
         local filename = "steps-" .. datestr .. ".json"
         t["filename"] = filename
-        SM("Initializing step count module: " .. datestr)
+        bmt:settext("✔ Step Count")
+    end,
+    LoadFont("Common Normal") .. {
+    Name = "Status",
+    InitCommand = function(self)
+      self:halign(0)
+      self:settext("❌ Step Count")
     end
+  },
 }
 
 t["ScreenEvaluationStage"] = Def.ActorFrame {
 	ModuleCommand=function(self)
-	    write_step_count(t["filename"])
-    end
+        write_step_count(t["filename"], self)
+    end,
+    LoadFont("Common Normal") .. {
+        Name = "CountP1",
+        InitCommand = function(self)
+            self:xy(10, _screen.h - 65):zoom(0.6):halign(0)
+            self:settext("❌")
+        end
+    },
+    LoadFont("Common Normal") .. {
+        Name = "CountP2",
+        InitCommand = function(self)
+            self:xy(_screen.w - 10, _screen.h - 65):zoom(0.6):halign(1)
+            self:settext("❌")
+        end
+    }
 }
 
 t["ScreenEvaluationSummary"] = Def.ActorFrame {
 	ModuleCommand=function(self)
-	    write_step_count(t["filename"])
-    end
+        write_step_count(t["filename"], self)
+    end,
+    LoadFont("Common Normal") .. {
+        Name = "CountP1",
+        InitCommand = function(self)
+            self:xy(10, _screen.h - 48):zoom(0.8):halign(0)
+            self:settext("")
+        end
+    },
+    LoadFont("Common Normal") .. {
+        Name = "CountP2",
+        InitCommand = function(self)
+            self:xy(_screen.w - 10, _screen.h - 48):zoom(0.8):halign(1)
+            self:settext("")
+        end
+    }
 }
 
 return t
